4667c575f7c7c0c806021816446b37636b7434c7
kalojo <kalokjo@gmail.com>
2025-10-07 10:10:15 -0600

    first commit

---
+++ extract_text.py
@@ -0,0 +1,13 @@
+import pypdf, sys
+
+def extract_text(pdf_path):
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text()
+        return text
+
+if __name__ == "__main__":
+    pdf_path = sys.argv[1]
+    print(extract_text(pdf_path))
